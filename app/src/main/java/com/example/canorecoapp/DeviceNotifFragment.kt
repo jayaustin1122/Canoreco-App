eab2f6ebb1e022d71bf81b84cec7e7672deb9ef9
unknown <jayaustin1122>
2024-11-12 23:32:26 +0800

    Added Some

--- app/src/main/java/com/example/canorecoapp/DeviceNotifFragment.kt
+++ app/src/main/java/com/example/canorecoapp/DeviceNotifFragment.kt
@@ -1,5 +1,15 @@
 package com.example.canorecoapp
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.telephony.SmsManager
 import android.util.Log
@@ -7,6 +17,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import com.example.canorecoapp.databinding.FragmentDeviceNotifBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -15,9 +29,13 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ListenerRegistration
 import pub.devrel.easypermissions.EasyPermissions
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class DeviceNotifFragment : Fragment(), EasyPermissions.PermissionCallbacks {
     private lateinit var binding: FragmentDeviceNotifBinding
@@ -54,6 +72,115 @@ class DeviceNotifFragment : Fragment(), EasyPermissions.PermissionCallbacks {
         startListeningForDeviceStatuses()
         startListeningForSms()
 
+    }
+    private fun startListeningForNotifications() {
+        val userId = auth.currentUser?.uid ?: return
+        val notificationsRef = db.collection("users")
+            .document(userId)
+            .collection("notifications")
+
+        notificationsListener = notificationsRef.addSnapshotListener { snapshot, exception ->
+            if (exception != null) {
+                // Handle error here
+                return@addSnapshotListener
+            }
+
+            snapshot?.let { snap ->
+                for (document in snap.documentChanges) {
+                    if (document.type == com.google.firebase.firestore.DocumentChange.Type.ADDED) {
+                        val doc = document.document
+                        val title = doc.getString("title") ?: "No Title"
+                        val timestamp = doc.get("timestamp")
+                        val isRead = doc.getBoolean("isRead") ?: false
+
+                        if (!isRead) {
+                            val notificationsRef2 = db.collection("users")
+                                .document(userId)
+                                .collection("notifications")
+                                .document(timestamp.toString())
+                            createNotification(title, timestamp.toString(),notificationsRef2,isRead)
+                            notificationsRef2.update("isRead", true).addOnCompleteListener { updateTask ->
+                                if (!updateTask.isSuccessful) {
+                                    Log.e("NotificationService", "Failed to update isRead status", updateTask.exception)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    @SuppressLint("SimpleDateFormat")
+    private fun parseAndFormatDate(timestampString: String): String {
+        return try {
+
+            val timestamp = timestampString.toDoubleOrNull()?.toLong() ?: return ""
+            val date = Date(timestamp * 1000)
+            val outputFormat = SimpleDateFormat("MMMM d, yyyy h:mm a", Locale.getDefault())
+            outputFormat.format(date)
+        } catch (e: NumberFormatException) {
+            Log.e("Home", "Number format error: ", e)
+            ""
+        }
+    }
+    private fun createNotification(
+        title: String,
+        timestamp: String,
+        notificationsRef2: DocumentReference,
+        isRead: Boolean
+    ) {
+        val channelId = "test_channel_id"
+        val channelName = "Test Channel"
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val notificationChannel = NotificationChannel(channelId, channelName, importance).apply {
+                description = "Channel description"
+                enableLights(true)
+                lightColor = Color.RED
+                enableVibration(true)
+                vibrationPattern = longArrayOf(0, 500, 1000)
+            }
+            val notificationManager = context?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+        val intent = Intent(requireContext(), MainActivity::class.java).apply {
+            updateIsRead(notificationsRef2,isRead)
+            putExtra("navigate_to_fragment", "YourFragmentTag")
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+
+        }
+        val pendingIntent = PendingIntent.getActivity(requireContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
+
+
+        val notificationBuilder = NotificationCompat.Builder(requireContext(), channelId)
+            .setSmallIcon(R.drawable.logo)
+            .setContentTitle(title)
+            .setContentText(parseAndFormatDate(timestamp))
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+            .setOngoing(false)
+            .setVibrate(longArrayOf(0, 500, 1000))
+            .setContentIntent(pendingIntent)
+
+        with(NotificationManagerCompat.from(requireContext())) {
+            if (ActivityCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return
+            }
+            notify(1, notificationBuilder.build())
+        }
+    }
+
+    private fun updateIsRead(notificationsRef2: DocumentReference, isRead: Boolean) {
+        notificationsRef2.update("isRead", true).addOnCompleteListener { updateTask ->
+            if (!updateTask.isSuccessful) {
+                Log.e("NotificationService", "Failed to update isRead status", updateTask.exception)
+            }
+        }
     }
 
     private fun startListeningForSms() {
@@ -193,7 +320,7 @@ class DeviceNotifFragment : Fragment(), EasyPermissions.PermissionCallbacks {
                     .set(notificationData)
                     .addOnSuccessListener {
                         Log.d("DeviceNotifFragment", "Notification successfully added for user $userId")
-                        updateDeviceToWorking(id)
+
                     }
                     .addOnFailureListener { e ->
                         Log.e("DeviceNotifFragment", "Failed to add notification for user $userId", e)
